27debfa409b0d72c83bd0b5fe5d54aee8035da4e
Woohyuk Song <harrywsong@naver.com>
2025-09-19 12:54:57 -0400

    lab 3 update

---
+++ labs/lab3/index.js
@@ -0,0 +1,52 @@
+var http = require("http");
+//TODO - Use Employee Module here
+const employees = require('./Employee.js');
+
+console.log("Lab 03 -  NodeJs");
+
+//TODO - Fix any errors you found working with lab exercise
+
+//Define Server Port
+const port = process.env.PORT || 8081
+
+//Create Web Server using CORE API
+const server = http.createServer((req, res) => {
+    if (req.method !== 'GET') {
+        res.end(`{"error": "${http.STATUS_CODES[405]}"}`)
+    } else {
+        if (req.url === '/') {
+            //TODO - Display message "<h1>Welcome to Lab Exercise 03</h1>"
+            res.setHeader('Content-Type', 'text/html');
+            res.end("<h1>Welcome to Lab Exercise 03</h1>");
+            return;
+        }
+
+        if (req.url === '/employee') {
+            //TODO - Display all details for employees in JSON format
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(employees));
+            return;
+        }
+
+        if (req.url === '/employee/names') {
+            //TODO - Display only all employees {first name + lastname} in Ascending order in JSON Array
+            //e.g. [ "Ash Lee", "Mac Mohan", "Pritesh Patel"]
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(employees.map(employee => `${employee.firstName} ${employee.lastName}`).sort()));
+            return;
+        }
+
+        if (req.url === '/employee/totalsalary') {
+            //TODO - Display Sum of all employees salary in given JSON format 
+            //e.g. { "total_salary" : 100 } 
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ "total_salary": employees.reduce((sum, employee) => sum + employee.Salary, 0) }));
+            return;
+        }
+    res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
+    }
+})
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
